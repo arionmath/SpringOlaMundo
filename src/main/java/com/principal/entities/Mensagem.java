edc7dfc609308de087e8f77f0447e7e2cef851c4
Arion Mathias <arion.mathias@gmail.com>
2021-03-04 21:32:44 -0300

    Primeiro commit

---
+++ src/main/java/com/principal/entities/Mensagem.java
@@ -0,0 +1,26 @@
+package com.principal.entities;
+
+public class Mensagem {
+	private Long id;
+	private String corpo;
+	
+	
+	
+	
+	public String getCorpo() {
+		return corpo;
+	}
+
+	public void setCorpo(String corpo) {
+		this.corpo = corpo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
